48352eceedd0d25fa5b53ba9e3c74e451af1f8ff
suimenkathemove <yoshiharu2580@gmail.com>
2024-02-29 00:13:00 +0900

    cancel export notion-version

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,5 @@
 export * from "./types";
 export * from "./components/react-notion-sortable-tree";
-export * from "./components/notion-version";
 export * from "./utils/update-node";
 export * from "./utils/remove-node";
 export * from "./utils/move-node";
