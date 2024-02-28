2f6bebf31d1ec74f9db9998ca2e8c470f8fb888e
suimenkathemove <42399693+suimenkathemove@users.noreply.github.com>
2024-02-29 00:09:48 +0900

    add title to node data (#12)

--- src/__mocks__/flattened-tree.ts
+++ src/__mocks__/flattened-tree.ts
@@ -1,16 +1,16 @@
 import { FlattenedTreeItem } from "@/types";
 
-export const flattenedTree: FlattenedTreeItem<Record<string, unknown>>[] = [
-  { id: "1", parentId: null, depth: 0, collapsed: true, data: {} },
-  { id: "4", parentId: "1", depth: 1, collapsed: true, data: {} },
-  { id: "10", parentId: "4", depth: 2, collapsed: true, data: {} },
-  { id: "11", parentId: "4", depth: 2, collapsed: true, data: {} },
-  { id: "12", parentId: "4", depth: 2, collapsed: true, data: {} },
-  { id: "5", parentId: "1", depth: 1, collapsed: true, data: {} },
-  { id: "6", parentId: "1", depth: 1, collapsed: true, data: {} },
-  { id: "2", parentId: null, depth: 0, collapsed: true, data: {} },
-  { id: "7", parentId: "2", depth: 1, collapsed: true, data: {} },
-  { id: "8", parentId: "2", depth: 1, collapsed: true, data: {} },
-  { id: "9", parentId: "2", depth: 1, collapsed: true, data: {} },
-  { id: "3", parentId: null, depth: 0, collapsed: true, data: {} },
+export const flattenedTree: FlattenedTreeItem<{ title: string }>[] = [
+  { id: "1", parentId: null, depth: 0, collapsed: true, data: { title: "1" } },
+  { id: "4", parentId: "1", depth: 1, collapsed: true, data: { title: "4" } },
+  { id: "10", parentId: "4", depth: 2, collapsed: true, data: { title: "10" } },
+  { id: "11", parentId: "4", depth: 2, collapsed: true, data: { title: "11" } },
+  { id: "12", parentId: "4", depth: 2, collapsed: true, data: { title: "12" } },
+  { id: "5", parentId: "1", depth: 1, collapsed: true, data: { title: "5" } },
+  { id: "6", parentId: "1", depth: 1, collapsed: true, data: { title: "6" } },
+  { id: "2", parentId: null, depth: 0, collapsed: true, data: { title: "2" } },
+  { id: "7", parentId: "2", depth: 1, collapsed: true, data: { title: "7" } },
+  { id: "8", parentId: "2", depth: 1, collapsed: true, data: { title: "8" } },
+  { id: "9", parentId: "2", depth: 1, collapsed: true, data: { title: "9" } },
+  { id: "3", parentId: null, depth: 0, collapsed: true, data: { title: "3" } },
 ];
