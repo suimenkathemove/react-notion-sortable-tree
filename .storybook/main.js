82ceb89a80a3fa1ba377e8ce1b2e3271eed244a6
suimenkathemove <yoshiharu2580@gmail.com>
2024-03-08 08:40:53 +0900

    refactor storybook addon

--- .storybook/main.js
+++ .storybook/main.js
@@ -3,9 +3,9 @@ const path = require("path");
 module.exports = {
   stories: ["../src/**/*.stories.tsx"],
   addons: [
-    "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
+    "@storybook/addon-links",
   ],
   framework: {
     name: "@storybook/react-webpack5",
