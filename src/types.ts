8536d051e41bd9fab014e355368da3275fe3dc12
suimenkathemove <yoshiharu2580@gmail.com>
2024-07-11 12:10:11 +0900

    Revert "fix"
    This reverts commit b4cf224a49de2fd5061c1ee690b02d12e8c8f390.

---
+++ src/types.ts
@@ -0,0 +1,23 @@
+export type NodeId = string;
+
+export type Node<T extends Record<string, unknown>> = {
+  id: NodeId;
+  children: Node<T>[];
+  collapsed: boolean;
+  data: T;
+};
+
+export type Tree<T extends Record<string, unknown>> = Node<T>[];
+
+export type FlattenedTreeItem<T extends Record<string, unknown>> = {
+  id: NodeId;
+  parentId: NodeId | null;
+  depth: number;
+  collapsed: boolean;
+  data: T;
+};
+
+export type MoveTarget = {
+  type: "parent" | "siblingParent" | "siblingChild";
+  id: NodeId;
+};
