d4e16b21621d5034cb843a7c2755a02897562222
suimenkathemove <42399693+suimenkathemove@users.noreply.github.com>
2024-07-17 16:46:51 +0900

    refactor addNode (#29)

--- src/utils/add-node-to-parent/index.ts
+++ src/utils/add-node-to-parent/index.ts
@@ -1,5 +1,3 @@
-import { invariant } from "@suimenkathemove/utils";
-
 import { Node, NodeId, Tree } from "@/types";
 
 export const addNodeToParent = <T extends Record<string, unknown>>(
@@ -7,50 +5,18 @@ export const addNodeToParent = <T extends Record<string, unknown>>(
   targetId: NodeId,
   newNode: Node<T>,
 ): Tree<T> => {
-  const newTree: Tree<T> = tree.map((node) => ({
-    id: node.id,
-    children: [],
-    collapsed: node.collapsed,
-    data: node.data,
-  }));
-  const nodeMap: Record<NodeId, Node<T>> = newTree.reduce(
-    (acc, node) => ({ ...acc, [node.id]: node }),
-    {},
-  );
+  const dfs = (nodes: Tree<T>): Tree<T> =>
+    nodes.reduce((acc, node) => {
+      const n: Node<T> = {
+        ...node,
+        children:
+          node.id === targetId
+            ? [...dfs(node.children), newNode]
+            : dfs(node.children),
+      };
 
-  const dfs = (original: Node<T>, parentId: NodeId): void => {
-    const node: Node<T> = {
-      id: original.id,
-      children: [],
-      collapsed: original.collapsed,
-      data: original.data,
-    };
-    nodeMap[node.id] = node;
+      return [...acc, n];
+    }, [] as Tree<T>);
 
-    const parent = nodeMap[parentId];
-    invariant(parent != null, "parent should exist");
-    parent.children.push(node);
-
-    original.children.forEach((c) => {
-      dfs(c, original.id);
-    });
-
-    if (original.id === targetId) {
-      node.children.push(newNode);
-    }
-  };
-
-  tree.forEach((original) => {
-    original.children.forEach((c) => {
-      dfs(c, original.id);
-    });
-
-    if (original.id === targetId) {
-      const targetNode = nodeMap[targetId];
-      invariant(targetNode != null, "targetNode should exist");
-      targetNode.children.push(newNode);
-    }
-  });
-
-  return newTree;
+  return dfs(tree);
 };
